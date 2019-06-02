1d4d566d5d11e1e84e412d83e36b0da13dfb53fa
DianaCaraian <dianacaraian1@gmail.com>
2019-06-02 15:04:42 +0300

    rezolvare erori din main

--- proiectCTP/ui.h
+++ proiectCTP/ui.h
@@ -16,9 +16,10 @@ public:
 
 	void print_bus();
 	void show_tickets(std::string id_wanted);
-	void buy_tickets(std::string id_wanted,zona& z,zona& z2,std::string date);
+	bool buy_tickets(std::string id_wanted,zona& z,zona& z2,std::string date);
 	//std::vector<bus> get_fastest_route();
 	//std::vector<bus> get_cheapest_route();
+	std::vector<bus> get_fastest_route(std::string id_wanted, zona& z, zona& z2, std::string date);
 	bool logare(std::string &iduser, std::string &parolauser);
 	void read_data_4buy( zona& z1, zona& z2, std::string& date);
 };
